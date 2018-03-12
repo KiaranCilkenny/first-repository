71c88d27060f7f421efa2f5dc35eec3d2e890b4d
CIARAN KILKENNY - Student <G00334394@gmit.ie>
2018-03-12 11:53:27 +0000

    Lab March 12th

--- Index.java
+++ Index.java
@@ -5,12 +5,16 @@ public class Index{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
-    System.out.println("Select an option (1-3):");
+    System.out.println("Select an option (1-7):");
     System.out.println();
     System.out.println("1. Break");
     System.out.println("2. Switch");
     System.out.println("3. Methods");
-    System.out.println("4. Exit");
+    System.out.println("4. Shadowing");
+    System.out.println("5. Overloading");
+    System.out.println("6. Reverse Digits");
+    System.out.println("7. Exit");
+
 
     System.out.println();
 
@@ -34,6 +38,21 @@ public class Index{
         Methods.main(null);
         break;
       case 4:
+	    System.out.println("Shadowing");
+	    System.out.println();
+	    Shadow.main(null);
+        break;
+     case 5:
+	 	System.out.println("Overloading");
+	 	System.out.println();
+	 	Arithmetic.main(null);
+        break;
+     case 6:
+	 	System.out.println("Reverse Digits");
+	 	System.out.println();
+	 	Reverse.main(null);
+        break;
+     case 7:
         System.out.println("Exiting....");
         break;
       default:
